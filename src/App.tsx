2b008a0608d4001fe589f81047b4f2ce2f405f26
Sanjay Verma <sanjay.verma@nxtwave.co.in>
2023-05-08 14:00:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SplitPane from "react-split-pane";
 
 function App() {
+  const splitChildren = () => {
+    return (
+      <>
+        <div>Hii</div>
+        <div>Hello</div>
+      </>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //@ts-ignore
+    <SplitPane split="vertical" minSize={200} defaultSize={200} maxSize={800}>
+      <div>Div 1</div>
+      <div>Div 2</div>
+    </SplitPane>
   );
 }
 
